b4d67b4a4b5c967457ec5e0b66c9aff8c7f5745b
Pavel Puchinski <pavel.puchinsky@gmail.com>
2020-12-19 18:13:14 +0300

    new commit day16-17

---
+++ pave_puchinski/day16/src/main/java/by/day16/Task.java
@@ -0,0 +1,2 @@
+package by.day16;public class Task {
+}
---
+++ pave_puchinski/day16/src/main/java/by/day16/task2/Task2.java
@@ -0,0 +1,16 @@
+package by.day16;
+
+import java.io.File;
+
+public class Task2 {
+    public static void main(String[] args) {
+        File file = new File("file1.txt");
+        File file2 = new File("file2.txt");
+
+
+
+
+    }
+
+
+}
